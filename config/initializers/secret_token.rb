e9b03c060c01bc49958f2f93875db6b24c26deaa
Ketan <katkarketan94@users.noreply.github.com>
2014-12-20 13:30:48 +0530

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Facebooklikes::Application.config.secret_token = '12495fdcc28faa5af7b6a6dd148669181ded395630e5d99696453f5f8e101ad6f8e33ee5072899fd9aa7cb9c1d7d83f2eed5a04cd85ae2cba9737d0392083e34'
+Facebooklikes::Application.config.secret_token = '#SECRET_KEY'
